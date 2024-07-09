a54f8d134eb0e2f6d80e3cf0163a0cbca9c4960c
yeseul <from.yeseul@gmail.com>
2024-07-09 11:18:47 +0900

    마무리완료

--- src/assets/js/smooth.js
+++ src/assets/js/smooth.js
@@ -1,3 +1,14 @@
-export function smooth() {
-    console.log(smooth)
+import Lenis from "@studio-freight/lenis";
+
+export function smooth(){
+    const lenis = new Lenis()
+
+    lenis.on("scroll", (e) => {
+        console.log(e)
+    })
+    function raf(time){
+        lenis.raf(time)
+        requestAnimationFrame(raf);
+    }
+    requestAnimationFrame(raf);
 }
